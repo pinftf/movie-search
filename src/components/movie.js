4dad72a62d8484a673ee5c32d9b58a6da0bd5e4b
Joao Pinto <pinftf@gmail.com>
2023-10-30 06:32:22 +0000

    Feat: adds movie card

---
+++ src/components/movie.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const DEFAULT_PLACEHOLDER_IMAGE =
+  'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg'
+
+const Movie = ({ movie }) => {
+  const poster =
+    movie.Poster === 'N/A' ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster
+  return (
+    <div className="movie">
+      <h2>{movie.Title}</h2>
+      <div>
+        <img
+          width="200"
+          alt={`The movie titled: ${movie.Title}`}
+          src={poster}
+        />
+      </div>
+      <p>({movie.Year})</p>
+    </div>
+  )
+}
+
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Poster: PropTypes.string.isRequired,
+    Year: PropTypes.string.isRequired
+  }).isRequired
+}
+
+export default Movie
