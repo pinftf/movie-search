c0c36b2808c04b7bf60aa5773972252d16a7c85c
Joao Pinto <pinftf@gmail.com>
2023-11-01 15:22:44 +0000

    Feat: adds movie card

---
+++ src/components/primitive components/Movie.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const CUSTOM_PLACEHOLDER_IMAGE =
+  'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg'
+
+const Movie = ({ movie }) => {
+  const poster =
+    movie.Poster === 'N/A' ? CUSTOM_PLACEHOLDER_IMAGE : movie.Poster
+  return (
+    <div className="movie">
+      <h2>{movie.Title}</h2>
+      <div>
+        <img
+          width="200"
+          alt={`The movie titled: ${movie.Title}`}
+          src={poster}
+        />
+      </div>
+      <p>({movie.Year})</p>
+    </div>
+  )
+}
+
+export default Movie
